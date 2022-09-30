9c4a812a0adc53a0c86603f04588db0a27a2a090
KiyonamiYu <yuqingbo0122@gmail.com>
2022-09-30 22:08:50 +0800

    fix(cmd.ts): some bugs about command options

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,25 +1,31 @@
 import { program } from "commander";
 
 export interface Cmd {
-  classpath?: boolean;
+  classpath?: string;
   classname?: string;
   args?: string[];
 }
 
 export function parseCmd(): Cmd {
   const cmd: Cmd = {};
-  // default have `-h` option
+  // default has `-h` option
   program
     .version("0.0.1", "-v, --version", "print the current version") // TODO read version from package.json
-    .option("--classpath <cp>", "classpath")
+    .option("-cp, --classpath <cp>", "classpath")
     .argument("<class>", "class")
-    .argument("[args]", "args")
+    .argument("[args...]", "args")
     .action((classname, args) => {
       cmd.classname = classname;
       if (args != null) {
         cmd.args = args;
       }
     });
+
   program.parse(process.argv);
+
+  const options = program.opts<{ classpath: string }>();
+  if (options.classpath != null) {
+    cmd.classpath = options.classpath;
+  }
   return cmd;
 }
